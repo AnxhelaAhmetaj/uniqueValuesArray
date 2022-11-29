fac2924911acbab4bb17fdca210017002b81b014
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-11-29 15:27:38 +0100

    clean code

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import fsp from 'fs/promises';
 
-async function createArray(content) {
+async function findUniqueValuesInFile(content) {
     const data = content.split('\n');
     const uniqueValues = data.filter((value, index, array) => array.indexOf(value) === index);
-    
-    // await fsp.writeFile('result.txt', uniqueValues);
-   console.log(uniqueValues);
+    console.log(uniqueValues);
 }
 
- try {
-        const content = await fsp.readFile('./file.csv', 'utf-8');
-       // console.log(content);
-        await createArray(content); 
-    }
-catch  (error) {
+try {
+    const content = await fsp.readFile('./data_file.csv', 'utf-8');
+    await findUniqueValuesInFile(content); 
+}
+catch (error) {
     console.log(error.message); 
 }
